05d718e7006f400eec8195ea4904017feaf43d61
Danny <drfritz@csbsju.edu>
2019-03-12 12:09:19 -0500

    First commit.

---
+++ main.py
@@ -0,0 +1,64 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_flex_quickstart]
+import logging
+
+from flask import Flask, request, make_response, jsonify
+import pymongo
+
+app = Flask(__name__)
+client = pymongo.MongoClient("mongodb+srv://admin:password1234@cluster0-bzguy.gcp.mongodb.net/test?retryWrites=true")
+# function for responses
+def results():
+    # build a request object
+    req = request.get_json(force=True)
+
+    # fetch meal from json
+    meal = req.get('queryResult').get('queryText')
+    #data = scrape(meal, date)
+    db = client.test
+    collection = db['menu_data']
+    data = collection.find_one({"meal":meal})
+    meta_data = ['_id', 'date', 'meal']
+    if len(data) > 0:
+        build_str = ''
+        for i in data:
+            if i not in meta_data:
+                build_str += data.get(i) + ", "
+        #return {'fulfilmentText': 'Today for ' + meal + ', the reef will be serving' + build_str}
+        return {'fulfilmentText': 'Today for ' + meal + ', the reef will be serving'}
+    # return a fulfillment response
+    else:
+        return {'fulfillmentText': 'Sorry, couldn\'t find any information on that.'}
+
+# create a route for webhook
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # return response
+    return make_response(jsonify(results()))
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_flex_quickstart]
---
+++ scraper.py
@@ -0,0 +1,106 @@
+import requests
+from requests.exceptions import RequestException
+from contextlib import closing
+from selenium import webdriver
+#from pyvirtualdisplay import Display
+import time
+import datetime
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://admin:password1234@cluster0-bzguy.gcp.mongodb.net/test?retryWrites=true")
+db = client.test
+collection = db.menu_data
+
+def main():
+    #date = 'Monday, March 11, 2019'
+    date = get_date_today()
+    data = scrape('breakfast', date)
+    test_id = collection.insert_one(data)
+    data = scrape('lunch', date)
+    test_id = collection.insert_one(data)
+    data = scrape('dinner', date)
+    test_id = collection.insert_one(data)
+
+def get_date_today():
+    day = datetime.datetime.today().day
+    year = datetime.datetime.today().year
+    weekday = ''
+    month = ''
+
+    if(datetime.datetime.today().month == 1):
+        month = 'January'
+    elif(datetime.datetime.today().month == 2):
+        month = 'February'
+    elif(datetime.datetime.today().month == 3):
+        month = 'March'
+    elif(datetime.datetime.today().month == 4):
+        month = 'April'
+    elif(datetime.datetime.today().month == 5):
+        month = 'May'
+    elif(datetime.datetime.today().month == 6):
+        month = 'June'
+    elif(datetime.datetime.today().month == 7):
+        month = 'July'
+    elif(datetime.datetime.today().month == 8):
+        month = 'August'
+    elif(datetime.datetime.today().month == 9):
+        month = 'September'
+    elif(datetime.datetime.today().month == 10):
+        month = 'October'
+    elif(datetime.datetime.today().month == 11):
+        month = 'November'
+    elif(datetime.datetime.today().month == 12):
+        month = 'December'
+    else:
+        month = ''
+
+    if(datetime.datetime.today().weekday() == 0):
+        weekday = 'Monday'
+    elif(datetime.datetime.today().weekday() == 1):
+        weekday = 'Tuesday'
+    elif(datetime.datetime.today().weekday() == 2):
+        weekday = 'Wednesday'
+    elif(datetime.datetime.today().weekday() == 3):
+        weekday = 'Thursday'
+    elif(datetime.datetime.today().weekday() == 4):
+        weekday = 'Friday'
+    elif(datetime.datetime.today().weekday() == 5):
+        weekday = 'Saturday'
+    elif(datetime.datetime.today().weekday() == 6):
+        weekday = 'Sunday'
+    else:
+        weekday = ''
+    date_str = weekday + ', ' + month + ' ' + str(day) + ', ' + str(year)
+    return date_str
+
+def scrape(meal, date):
+    quote_page = 'http://sjumenu.csbsju.edu/NetNutrition/1'
+    browser = webdriver.Chrome()
+    browser.get(quote_page)
+    elem = browser.find_element_by_class_name('cbo_nn_menuTable')
+    children = elem.find_elements_by_class_name('cbo_nn_menuCell')
+    resp_dict = {}
+    done = False
+    resp_dict['date'] = date
+    resp_dict['meal'] = meal
+    for child in children:
+        if done:
+            break
+        if date in child.text:
+            meal_links = child.find_elements_by_class_name('cbo_nn_menuLink')
+            for link in meal_links:
+                if meal.upper() in link.text:
+                    link.click()
+                    time.sleep(3)
+                    menu_items = browser.find_elements_by_class_name('cbo_nn_itemHover')
+                    count = 0
+                    for menu_item in menu_items:
+                        resp_dict[str(count)] = menu_item.text
+                        count+=1
+                    done = True
+                    break
+    browser.close()
+    return resp_dict
+
+if __name__ == "__main__":
+    main()
